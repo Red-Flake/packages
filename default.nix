794578335285fcf162ac8e9cbc3b0ab4d8aafa80
Julie B. <no-reply@bbjubjub.fr>
2024-05-18 19:03:22 +0200

    autocall packages under pkgs/

--- default.nix
+++ default.nix
@@ -8,13 +8,13 @@
 
 { pkgs ? import <nixpkgs> { } }:
 
-{
+# automatically call packages under pkgs/
+builtins.mapAttrs (name: _: pkgs.callPackage (./pkgs + "/${name}") {}) (builtins.readDir ./pkgs)
+// {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  # automatic packages can still be overriden
 }
